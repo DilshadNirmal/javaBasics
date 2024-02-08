d62c0ad6fa0dfd1fe2545bf188d93f355d71085a
DilshadNirmal <dilshadgov2020@gmail.com>
2024-02-08 15:57:42 +0530

    duplicate added

--- src/Array/OddOrEven.java
+++ src/Array/OddOrEven.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 
 public class OddOrEven {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 4, 8};
         ArrayList<Integer>[] result = numberFind(numbers);
+        ArrayList<Integer>[] duplicate = duplicate(numbers);
 
         System.out.println("Odd numbers:");
         for (int number : result[0]) {
@@ -16,6 +17,13 @@ public class OddOrEven {
 
         System.out.println("Even numbers:");
         for (int number : result[1]) {
+            System.out.print(number + " ");
+        }
+
+        System.out.println();
+
+        System.out.println("Duplicate numbers:");
+        for (int number : duplicate[0]) {
             System.out.print(number + " ");
         }
     }
@@ -37,5 +45,24 @@ public class OddOrEven {
         result[0] = oddNumbers;
         result[1] = evenNumbers;
         return result;
+    }
+
+    public static ArrayList<Integer>[] duplicate(int[] arr) {
+
+        ArrayList<Integer> duplicate = new ArrayList<>();
+
+        for (int index = 0; index < arr.length; index++) {
+            for (int j = index + 1; j < arr.length; j++) {
+                if (arr[index] == arr[j]) {
+                    duplicate.add(arr[index]);
+                }
+            }
+        }
+
+        @SuppressWarnings("unchecked")
+        ArrayList<Integer>[] duplicates = new ArrayList[1];
+        duplicates[0] = duplicate;
+
+        return duplicates;
     }
 }
