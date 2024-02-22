ba417e0cc4faace5fece07f90a09e4586863570c
DilshadNirmal <dilshadgov2020@gmail.com>
2024-02-22 10:58:08 +0530

    some box obtained

--- src/Pattern/RandPattern.java
+++ src/Pattern/RandPattern.java
@@ -4,29 +4,34 @@ public class RandPattern {
     public static void main(String[] args) {
         int length = 10;
 
-        for (int i = 0; i < length / 2; i++) {
-            for (int j = 0; j < length  - 1; j++) {
-                if (i % 2 == 0) {
-                    if (j % 2 == 0) {
-                        System.out.print("*");
-                    } else {
-                        System.out.print(" ");
-                    }
-                }
-                if (i % 2 != 0) {
-                    if (j % 2 != 0) {
-                        System.out.print("*");
-                    } else {
-                        System.out.print(" ");
-                    }
-                }
-            } System.out.println();
-        }
+        printRandomPattern(length);
+
+        
     }
     
     public static void printRandomPattern(int length) {
         for (int row = 0; row < length / 2; row++) {
-            for (int col = 0; col < )
+            for (int col = 0; col < length; col++) {
+                if (row == 0 || row == length - 1 || col == 0 || col == length - 1) {
+                    System.out.print("* ");
+                } else {
+                    // System.out.print(" ");
+                    if (row % 2 == 0) {
+                        if (col % 2 == 0) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                    }
+                    if (row % 2 != 0) {
+                        if (col % 2 != 0) {
+                            System.out.print("* ");
+                        } else {
+                            System.out.print("  ");
+                        }
+                    }
+                }
+            } System.out.println();
         }
     }
 }
