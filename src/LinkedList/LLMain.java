43e22d3629195799f874675ed6e723e7a2160a2f
DilshadNirmal <dilshadgov2020@gmail.com>
2024-03-04 16:36:48 +0530

    singleLL

---
+++ src/LinkedList/LLMain.java
@@ -0,0 +1,16 @@
+package LinkedList;
+
+public class LLMain {
+    public static void main(String[] args) {
+        SingleLL sll = new SingleLL();
+
+        sll.insert(5);
+        sll.insert(3);
+        sll.insert(6);
+        sll.insert(9);
+        sll.insert(4);
+        sll.insert(2);
+
+        sll.print();
+    }
+}
---
+++ src/LinkedList/SingleLLNode.java
@@ -0,0 +1,22 @@
+package LinkedList;
+
+public class SingleLLNode {
+    private int value;
+    private SingleLLNode next = null;
+
+    public SingleLLNode getNext() {
+        return next;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setNext(SingleLLNode next) {
+        this.next = next;
+    }
+}
